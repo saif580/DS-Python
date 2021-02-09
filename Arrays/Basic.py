7ddba0997f26cadd328e73b473edc51602775026
iamsaif <saifulhasan580@gmail.com>
2021-02-09 16:53:44 +0530

    list basic

--- Arrays/Basic.py
+++ Arrays/Basic.py
@@ -1,2 +1,32 @@
-a=[0,10,123,3,4,56,3,2356,34,34,34];
-print(len(a))
+import math
+
+# print(abs(-9));
+
+# print(pow(2,3));
+
+# print(min(2,3,12,3));
+
+# print(max(2,3,12,3));
+
+x="Hello my name is saif";
+y="I love coding!"
+
+print(x+'\t'+y)
+
+print(x[0:5])
+print(x[:5])
+
+#Clone the list
+z=x[:];
+print(z)
+
+print(x[:-1])
+
+print(x.count("s"))
+
+print(x.find("sa"))
+
+if x.endswith("f"):
+    print("Ends with O")
+else:
+    print("Does not end with O")
